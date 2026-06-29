0b38cbc55ab03784182193ede93b9a40387f9594
JUNIORDECHAVES <juniorchaves0010@gmail.com>
2026-06-29 09:28:09 -0300

    chore: remover build files

--- build/index.js
+++
@@ -1,5 +0,0 @@
-import { app } from "./server/server";
-app.listen(process.env.PORT || 3333, () => {
-    console.log('Server Rodando na porta 3333');
-});
-//# sourceMappingURL=index.js.map
--- build/server/server.d.ts
+++
@@ -1,5 +0,0 @@
-import { type Express } from 'express';
-import "dotenv/config";
-declare const app: Express;
-export { app };
-//# sourceMappingURL=server.d.ts.map
