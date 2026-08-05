f5164aa48592c45ff3347f85fe8b73b6a1fa560d
JUNIORDECHAVES <juniorchaves0010@gmail.com>
2026-08-05 10:38:35 -0300

    feat: added cors configuretion

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,12 @@
 import express, { type Express } from 'express';
+import cors from 'cors';
 import { router } from "./routes/index.js";
 import  "dotenv/config";
 
 const app: Express = express();
+app.use(cors({
+    origin: process.env.ENABLED_CORS?.split(";") || [],
+}));
 app.use(express.json());
 app.use(router);
 app.set('query parser', 'extended');
